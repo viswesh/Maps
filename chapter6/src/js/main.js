3ba1d4bf7152da4f59a1818b0003d16c41edfea4
Viswesh <ns.viswesh@gmail.com>
2017-12-17 18:39:23 -0800

    Augmented Reality (AR) with ar.js chapter
    Augmented Reality (AR) with ar.js chapter

---
+++ chapter6/src/js/main.js
@@ -0,0 +1,69 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2l0eXZpcyIsImEiOiJjajl4MGo0OWowczk4MnFtZHVob3I0MTdnIn0.KHhjbfQIuxBcoOsukPTldQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v9',
+    center: [0, 0],
+    maxBounds: [[-180, -85], [180, 85]],
+    zoom: 1
+});
+
+var url = 'http://localhost:5000/';
+
+map.on('load', function () {
+
+    window.setInterval(function () {
+        fetch(url).then(function (response) {
+            return response.json();
+        })
+            .then(function (json) {
+                var data = json,
+                    issLastSeen = data.features[0].geometry.coordinates,
+                    details = data.features[0].properties,
+                    resultingDOM = "",
+                    resultingText="";
+
+                for (var prop in details) {
+                    resultingDOM += "<span class='title'>" + prop.toUpperCase() + "</span>" + " " + details[prop] + "</br>";
+                    resultingText += `${prop.toUpperCase()}  ${details[prop]}`;
+                }
+
+                document.getElementById('details').innerHTML = resultingDOM;
+                document.getElementById('detailsEntity').setAttribute("text", "value: "+resultingText);
+                document.getElementById('locate').setAttribute("data-coordinate", JSON.stringify(issLastSeen));
+
+                map.getSource('iss').setData(data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }, 2000);
+
+    map.addSource('iss', {type: 'geojson', data: url});
+    map.addLayer({
+        "id": "iss",
+        "type": "symbol",
+        "source": "iss",
+        "layout": {
+            "icon-image": "rocket-15"
+        }
+    });
+
+    map.addControl(new mapboxgl.FullscreenControl());
+
+    document.getElementById('locate').addEventListener('click', function (e) {
+        var lastSeenLocaton = JSON.parse(this.getAttribute('data-coordinate'));
+        map.flyTo({
+            center: lastSeenLocaton
+        });
+    });
+
+});
+
+map.on('load', function () {
+    var canvas = document.querySelector("#map canvas"),
+        image = canvas.toDataURL("image/png"),
+        sphere = document.getElementById("sphere");
+
+    sphere.setAttribute("src", image);
+});
